f3de9b357b582791265bd1bc157e5fbe955a9649
norwayinnova <norwayinnova@gmail.com>
2026-09-17 01:55:05 +0200

    Add files via upload

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,13 +4,12 @@ import { getStorage } from 'firebase/storage';
 import { Platform } from 'react-native';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD8RcGHihBLmx2rU8VEa4ede5r6DDFiHRE",
-  authDomain: "newagain-80e25.firebaseapp.com",
-  projectId: "newagain-80e25",
-  storageBucket: "newagain-80e25.firebasestorage.app",
-  messagingSenderId: "695025623906",
-  appId: "1:695025623906:web:1e048ab5bd60ac3e95c23b",
-  measurementId: "G-V203ED9TR5"
+  apiKey: "AIzaSyAyZYx90mRTu9SfYkmue4oraMiOx-Vfldw",
+  authDomain: "avalonmystic-707e6.firebaseapp.com",
+  projectId: "avalonmystic-707e6",
+  storageBucket: "avalonmystic-707e6.firebasestorage.app",
+  messagingSenderId: "197654005050",
+  appId: "1:197654005050:web:9e860dae88e856d6c0fc03"
 };
 
 export const app = initializeApp(firebaseConfig);
