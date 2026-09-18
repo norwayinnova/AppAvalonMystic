631c78002317d8536a993bcb17b36b9051a0cb62
norwayinnova <norwayinnova@gmail.com>
2026-09-18 19:51:06 +0200

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,5 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import AppNavigator from './src/navigation/AppNavigator';
+import { enableScreens } from 'react-native-screens';
+
+// Disable native screens on web to avoid createScreenFactory crash
+enableScreens(false);
 
 import { AppProvider } from './src/context/AppContext';
 
